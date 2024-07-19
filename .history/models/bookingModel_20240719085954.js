dd0939d86f3f51349e3cdd5f181c41b9c254e48d
NhatThanh <duongnhatthanhaz09@gmail.com>
2024-07-19 09:00:08 +0700

    Test stripe

--- .history/models/bookingModel_20240719085954.js
+++ .history/models/bookingModel_20240719085954.js
@@ -7,6 +7,7 @@ const bookingSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
+    required: [true, "Booking must belong to a user"],
   },
   price: {
     type: Number,
